efcffb3949e6181be5b3dc63ec8afd7139eae482
noor <noortshepo@gmail.com>
2021-08-28 22:53:49 +0200

    added home page

--- src/ui/Layout/Layout.js
+++ src/ui/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, HStack, Button, } from "@chakra-ui/react";
 import * as Styles from "./Layout.module.css";
 
 import Navigation from "../../components/Navigation/Navigation";
@@ -10,7 +10,8 @@ import Left from "../../containers/Left/Left";
 const Layout = () => {
     return (
         <ChakraProvider>
-            <div className={Styles.Layout}>
+            <Navigation />
+            {/* <div className={Styles.Layout}>
                 <Navigation />
                 <div className={Styles.Body}>
                     <div className={Styles.Left} >
@@ -20,8 +21,32 @@ const Layout = () => {
                     <div className={Styles.Right}>
                     </div>
                 </div>
-
                 <div className={Styles.Footer}>Built by Mosia Noor-Tshepo</div>
+            </div> */}
+
+
+            <div className={Styles.Home}>
+                <div className={Styles.TextContainer}>
+                    <div className={Styles.BigText}>
+                        Connect with all your tutors online.
+                    </div>
+                    <div className={Styles.MediumText}>
+                        Teal Tutors connects students with tutors world
+                        wide. Providing resources and allowing them to
+                        have real-time lessons through live streams.
+                    </div>
+
+                    <div className={Styles.Buttons}>
+                        <HStack >
+                            <Button size="lg">
+                                Demo
+                            </Button>
+                            <Button size="lg" colorScheme="twitter" >
+                                Twitter
+                            </Button>
+                        </HStack>
+                    </div>
+                </div>
             </div>
         </ChakraProvider>
     );
